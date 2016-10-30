5708204e726626fa8f57fdc65a98ebd9c7ac52b1
Matthew Ariel <matthew_ariel_wangsit@yahoo.com>
2016-10-30 17:34:32 +0700

    Character Jump scipt added
    menambahkan script jump karakter

---
+++ GameDev/Assets/Script/CharacterScript/MainCharacter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacter : MonoBehaviour {
+	public Vector2 jumpPower = new Vector2(0,200);
+	public bool grounded = true;
+	public Rigidbody2D rb;
+
+	void start(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("up") && grounded == true) {
+			rb.AddForce (jumpPower);
+			grounded = false;
+		}
+
+	}
+
+	//@Override
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "ground"){
+			grounded = true;
+		}
+	}
+}
--- GameDev/Assets/Script/ObstacleScript/Obstacle.cs
+++ GameDev/Assets/Script/ObstacleScript/Obstacle.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class Obstacle : MonoBehaviour {
-	public Vector2 velocity = new Vector2 (Time.time * 0.3f, 0);
+	public Vector2 velocity = new Vector2 (Time.time * 0.1f, 0);
+	public Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = velocity;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (transform.position.x < -10) {
+			Destroy (gameObject);
+		}
 	}
 }
