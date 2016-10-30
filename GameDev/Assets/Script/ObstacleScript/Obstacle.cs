6e46f4cf4dd9c023584f4494daab275f9539fee9
Matthew Ariel <matthew_ariel_wangsit@yahoo.com>
2016-10-30 16:16:24 +0700

    Obstacle Generator and Obstacles
    menambahkan fitur Obstacle dan Obstacle generator

---
+++ GameDev/Assets/Script/ObstacleScript/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+	public Vector2 velocity = new Vector2 (Time.time * 0.3f, 0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
